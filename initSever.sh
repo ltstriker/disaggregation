63b0d0a4d19bb89cd7d2a12d0c9f24f695c588d8
ltstriker <ltuse163@163.com>
2020-08-13 14:51:08 +0800

    Update initSever.sh

--- initSever.sh
+++ initSever.sh
@@ -17,3 +17,14 @@ wget https://archive.apache.org/dist/hadoop/common/hadoop-2.5.1/hadoop-2.5.1.tar
 tar -xzvf hadoop-2.5.1.tar.gz
 export PATH=$PWD/hadoop-2.5.1/bin:$PATH
 hadoop version
+
+cd /root
+mkdir spark-ec2
+echo 10.10.1.1 > /root/spark-ec2/masters
+echo 10.10.1.2 > /root/spark-ec2/slaves
+
+mkdir /root/hadoop-2.5.1/conf
+cp /root/hadoop-2.5.1/etc/hadoop/mapred-site.xml.template /root/hadoop-2.5.1/conf/mapred-site.xml
+cp /root/hadoop-2.5.1/etc/hadoop/core-site.xml /root/hadoop-2.5.1/conf/
+cp /root/hadoop-2.5.1/etc/hadoop/hdfs-site.xml /root/hadoop-2.5.1/conf/
+ln -s /root/hadoop-2.5.1 /root/ephemeral-hdfs
